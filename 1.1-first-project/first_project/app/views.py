4a3e42876af2ffc9666b5c852b63edb5f1e8ee57
Anastasia Lichman <l.nastya.a.00@gmail.com>
2023-02-25 16:32:44 +0300

    first commit

---
+++ 1.1-first-project/first_project/app/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().strftime('%H:%M:%S')
+    msg = f'Текущее время и дата: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    work_dir = os.listdir(path='.')
+    msg = f'Список файлов в рабочей директории: {work_dir}'
+    return HttpResponse(msg)
+    
